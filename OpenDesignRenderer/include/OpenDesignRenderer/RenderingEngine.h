e90abf5aa67f7bab8386d37cf115c9a18ff90b91
Matúš Ždanský <matuszdansky@gmail.com>
2022-09-30 20:10:21 +0700

    [Refactor] Add important comments to renderer class and rename function variables for clarity.

--- OpenDesignRenderer/include/OpenDesignRenderer/RenderingEngine.h
+++ OpenDesignRenderer/include/OpenDesignRenderer/RenderingEngine.h
@@ -14,15 +14,27 @@ public:
     //! Copy frame buffer to image.
     bool CopyFrameBuffer(Image& image) const;
 
-    //! Draw an image to the specified position on the frame buffer.
+    /*!
+        \brief Draw an image to the specified position on the frame buffer.
+        \param image The image to be drawn.
+        \param imagePosition The position in the frame buffer where the image will be drawn.
+        \param imageDimensions The dimensions of the drawn image. May cause the image to scale.
+    */
     bool Draw(
         const Image& image,
-        const PixelCoordinatesUnbounded& position,
-        const ImageDimensions& dimensions);
-    //! Draw a rectangle to the specified position on the frame buffer.
+        const PixelCoordinatesUnbounded& imagePosition,
+        const ImageDimensions& imageDimensions);
+    /*!
+        \brief Draw a rectangle to the specified position on the frame buffer.
+        \param rectanglePosition The position in the frame buffer where the rectangle will be drawn.
+        \param rectangleDimensions The dimensions of the drawn rectangle.
+        \param fillColor The fill color of the rectangle.
+        \param innerStrokeWidth Stroke width, the stroke will be rendered inside the rectange.
+        \param strokeColor Stoke color.
+    */
     bool DrawRectangle(
-        const PixelCoordinatesUnbounded& position,
-        const ImageDimensions& dimensions,
+        const PixelCoordinatesUnbounded& rectanglePosition,
+        const ImageDimensions& rectangleDimensions,
         const PixelColor& fillColor,
         uint32_t innerStrokeWidth,
         const PixelColor& strokeColor);
--- OpenDesignRenderer/src/RenderingEngine.cpp
+++ OpenDesignRenderer/src/RenderingEngine.cpp
@@ -16,21 +16,21 @@ bool odr::RenderingEngine::CopyFrameBuffer(Image& image) const {
 
 bool odr::RenderingEngine::Draw(
     const Image& image,
-    const PixelCoordinatesUnbounded& position,
-    const ImageDimensions& dimensions) {
-    const Image scaledImage = image.Scaled(dimensions);
+    const PixelCoordinatesUnbounded& imagePosition,
+    const ImageDimensions& imageDimensions) {
+    const Image scaledImage = image.Scaled(imageDimensions);
 
     const ImageDimensions& frameBufferDimensions = frameBuffer.GetDimensions();
 
-    const uint32_t fbLeft = std::max(position.left, 0);
-    const uint32_t fbRight = std::min(dimensions.width + position.left, frameBufferDimensions.width);
-    const uint32_t fbTop = std::max(position.top, 0);
-    const uint32_t fbBottom = std::min(dimensions.height + position.top, frameBufferDimensions.height);
+    const uint32_t fbLeft = std::max(imagePosition.left, 0);
+    const uint32_t fbRight = std::min(imageDimensions.width + imagePosition.left, frameBufferDimensions.width);
+    const uint32_t fbTop = std::max(imagePosition.top, 0);
+    const uint32_t fbBottom = std::min(imageDimensions.height + imagePosition.top, frameBufferDimensions.height);
 
     // For each pixel in the frame buffer where the image will be drawn
     for (uint32_t y = fbTop; y < fbBottom; y++) {
         for (uint32_t x = fbLeft; x < fbRight; x++) {
-            const PixelCoordinates imageCoords{ x - position.left, y - position.top };
+            const PixelCoordinates imageCoords{ x - imagePosition.left, y - imagePosition.top };
             const PixelCoordinates frameBufferCoordinates{ x, y };
 
             const PixelColor imageColor = scaledImage.GetColor(imageCoords);
@@ -48,29 +48,29 @@ bool odr::RenderingEngine::Draw(
 }
 
 bool odr::RenderingEngine::DrawRectangle(
-    const PixelCoordinatesUnbounded& position,
-    const ImageDimensions& dimensions,
+    const PixelCoordinatesUnbounded& rectanglePosition,
+    const ImageDimensions& rectangleDimensions,
     const PixelColor& fillColor,
     uint32_t innerStrokeWidth,
     const PixelColor& strokeColor) {
     const ImageDimensions& frameBufferDimensions = frameBuffer.GetDimensions();
 
-    const uint32_t fbLeft = std::max(position.left, 0);
-    const uint32_t fbRight = std::min(dimensions.width + position.left, frameBufferDimensions.width);
-    const uint32_t fbTop = std::max(position.top, 0);
-    const uint32_t fbBottom = std::min(dimensions.height + position.top, frameBufferDimensions.height);
+    const uint32_t fbLeft = std::max(rectanglePosition.left, 0);
+    const uint32_t fbRight = std::min(rectangleDimensions.width + rectanglePosition.left, frameBufferDimensions.width);
+    const uint32_t fbTop = std::max(rectanglePosition.top, 0);
+    const uint32_t fbBottom = std::min(rectangleDimensions.height + rectanglePosition.top, frameBufferDimensions.height);
 
     // For each pixel in the frame buffer where the image will be drawn
     for (uint32_t y = fbTop; y < fbBottom; y++) {
         for (uint32_t x = fbLeft; x < fbRight; x++) {
-            const PixelCoordinates rectangleCoords{ x - position.left, y - position.top };
+            const PixelCoordinates rectangleCoords{ x - rectanglePosition.left, y - rectanglePosition.top };
             const PixelCoordinates frameBufferCoordinates{ x, y };
 
             const bool areRectangleCoordsInInnerFill =
                 rectangleCoords.left < innerStrokeWidth ||
-                rectangleCoords.left >= dimensions.width - innerStrokeWidth ||
+                rectangleCoords.left >= rectangleDimensions.width - innerStrokeWidth ||
                 rectangleCoords.top < innerStrokeWidth ||
-                rectangleCoords.top >= dimensions.height - innerStrokeWidth;
+                rectangleCoords.top >= rectangleDimensions.height - innerStrokeWidth;
 
             const PixelColor rectangleColor = areRectangleCoordsInInnerFill
                 ? strokeColor
