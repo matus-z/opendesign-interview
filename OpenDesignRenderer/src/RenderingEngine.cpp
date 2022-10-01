9a92640c57945fa2bd64d71889e8608fdc86ee12
Matúš Ždanský <matuszdansky@gmail.com>
2022-10-01 13:24:57 +0700

    [Refactor] Cleanup code in RenderingEngine class.

--- OpenDesignRenderer/src/RenderingEngine.cpp
+++ OpenDesignRenderer/src/RenderingEngine.cpp
@@ -20,31 +20,30 @@ bool odr::RenderingEngine::Draw(
     const ImageDimensions& imageDimensions) {
     const Image scaledImage = image.Scaled(imageDimensions);
 
-    const ImageDimensions& frameBufferDimensions = frameBuffer.GetDimensions();
+    const ImageDimensions& fbDimensions = frameBuffer.GetDimensions();
 
     const uint32_t fbLeft = std::max(imagePosition.left, 0);
-    const uint32_t fbRight = std::min(imageDimensions.width + imagePosition.left, frameBufferDimensions.width);
+    const uint32_t fbRight = std::min(imageDimensions.width + imagePosition.left, fbDimensions.width);
     const uint32_t fbTop = std::max(imagePosition.top, 0);
-    const uint32_t fbBottom = std::min(imageDimensions.height + imagePosition.top, frameBufferDimensions.height);
+    const uint32_t fbBottom = std::min(imageDimensions.height + imagePosition.top, fbDimensions.height);
+
+    bool areAllColorsSet = true;
 
     // For each pixel in the frame buffer where the image will be drawn
     for (uint32_t y = fbTop; y < fbBottom; y++) {
         for (uint32_t x = fbLeft; x < fbRight; x++) {
-            const PixelCoordinates imageCoords{ x - imagePosition.left, y - imagePosition.top };
-            const PixelCoordinates frameBufferCoordinates{ x, y };
+            const PixelCoordinates imgCoords{ x - imagePosition.left, y - imagePosition.top };
+            const PixelCoordinates fbCoords{ x, y };
 
-            const PixelColor imageColor = scaledImage.GetColor(imageCoords);
-            const PixelColor frameBufferColor = frameBuffer.GetColor(frameBufferCoordinates);
-
+            const PixelColor imageColor = scaledImage.GetColor(imgCoords);
+            const PixelColor frameBufferColor = frameBuffer.GetColor(fbCoords);
             const PixelColor blendedPixelColor = PixelColor::Blend(frameBufferColor, imageColor);
-            const bool isColorSet = frameBuffer.SetColor(blendedPixelColor, frameBufferCoordinates);
-            if (!isColorSet) {
-                return false;
-            }
+
+            areAllColorsSet &= frameBuffer.SetColor(blendedPixelColor, fbCoords);
         }
     }
 
-    return true;
+    return areAllColorsSet;
 }
 
 bool odr::RenderingEngine::DrawRectangle(
@@ -53,37 +52,35 @@ bool odr::RenderingEngine::DrawRectangle(
     const PixelColor& fillColor,
     uint32_t innerStrokeWidth,
     const PixelColor& strokeColor) {
-    const ImageDimensions& frameBufferDimensions = frameBuffer.GetDimensions();
+    const ImageDimensions& fbDimensions = frameBuffer.GetDimensions();
 
     const uint32_t fbLeft = std::max(rectanglePosition.left, 0);
-    const uint32_t fbRight = std::min(rectangleDimensions.width + rectanglePosition.left, frameBufferDimensions.width);
+    const uint32_t fbRight = std::min(rectangleDimensions.width + rectanglePosition.left, fbDimensions.width);
     const uint32_t fbTop = std::max(rectanglePosition.top, 0);
-    const uint32_t fbBottom = std::min(rectangleDimensions.height + rectanglePosition.top, frameBufferDimensions.height);
+    const uint32_t fbBottom = std::min(rectangleDimensions.height + rectanglePosition.top, fbDimensions.height);
+
+    bool areAllColorsSet = true;
 
     // For each pixel in the frame buffer where the image will be drawn
     for (uint32_t y = fbTop; y < fbBottom; y++) {
         for (uint32_t x = fbLeft; x < fbRight; x++) {
-            const PixelCoordinates rectangleCoords{ x - rectanglePosition.left, y - rectanglePosition.top };
-            const PixelCoordinates frameBufferCoordinates{ x, y };
+            const PixelCoordinates rectCoords{ x - rectanglePosition.left, y - rectanglePosition.top };
+            const PixelCoordinates fbCoords{ x, y };
 
-            const bool areRectangleCoordsInInnerFill =
-                rectangleCoords.left < innerStrokeWidth ||
-                rectangleCoords.left >= rectangleDimensions.width - innerStrokeWidth ||
-                rectangleCoords.top < innerStrokeWidth ||
-                rectangleCoords.top >= rectangleDimensions.height - innerStrokeWidth;
+            const bool arerectCoordsInInnerFill =
+                rectCoords.left < innerStrokeWidth ||
+                rectCoords.left >= rectangleDimensions.width - innerStrokeWidth ||
+                rectCoords.top < innerStrokeWidth ||
+                rectCoords.top >= rectangleDimensions.height - innerStrokeWidth;
 
-            const PixelColor rectangleColor = areRectangleCoordsInInnerFill
-                ? strokeColor
-                : fillColor;
-            const PixelColor frameBufferColor = frameBuffer.GetColor(frameBufferCoordinates);
+            const PixelColor frameBufferColor = frameBuffer.GetColor(fbCoords);
+            const PixelColor blendedPixelColor = arerectCoordsInInnerFill
+                ? PixelColor::Blend(frameBufferColor, strokeColor)
+                : PixelColor::Blend(frameBufferColor, fillColor);
 
-            const PixelColor blendedPixelColor = PixelColor::Blend(frameBufferColor, rectangleColor);
-            const bool isColorSet = frameBuffer.SetColor(blendedPixelColor, frameBufferCoordinates);
-            if (!isColorSet) {
-                return false;
-            }
+            areAllColorsSet &= frameBuffer.SetColor(blendedPixelColor, fbCoords);
         }
     }
 
-    return true;
+    return areAllColorsSet;
 }
